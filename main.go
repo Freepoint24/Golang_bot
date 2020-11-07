7174fcff6db1ca26db6db4f9262a99194ef852bd
Freepoin24 <5443006@gmail.com>
2020-11-07 23:07:51 +0300

    0001

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+// Точка входа программы
+func main() {
+	botToken := "1454820573:AAH0prOuE7Wq_anwAi5YPmx_g1XHN_iMsbw"
+}
+
+// Запрос обновлений
+func main() {
+
+}
+
+// Ответ на обновления
+func respond() {
+
+}
---
+++ models.go
@@ -0,0 +1,15 @@
+package main
+
+type Update struct {
+	Update  int     `json:"update_id"`
+	Message Message `json:"massage"`
+}
+
+type Message struct {
+	Chat Chat   `json:"chat"`
+	Text string `json:"text"`
+}
+
+type Chat struct {
+	ChatId int `json:"chat"`
+}
